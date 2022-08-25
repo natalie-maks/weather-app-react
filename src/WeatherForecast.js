ae69338d662e551b011e374d8710e05cd7ded6ea
natalie-maks <natasha.maxymenko@gmail.com>
2022-08-25 15:20:43 +0100

    Changed return of map function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,7 +26,7 @@ export default function WeatherForecast(props) {
               </div>
             );
           } else {
-            return `Loading...`;
+            return null;
           }
         })}
       </div>
